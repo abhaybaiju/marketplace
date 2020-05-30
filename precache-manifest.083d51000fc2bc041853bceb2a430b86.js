cb0dffdf6a377998090d5c706c273b4bb6d51f7a
abhay.baiju <abhay0198@gmail.com>
2020-05-30 13:08:10 -0700

    Updates

--- precache-manifest.083d51000fc2bc041853bceb2a430b86.js
+++ precache-manifest.083d51000fc2bc041853bceb2a430b86.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b62764dd78b20bc47113d88cc9ddecae",
+    "revision": "f90ef9f5f26ca91daf0240faf8d3369a",
     "url": "/marketplace/index.html"
   },
   {
-    "revision": "531bb9bfcc5486eccaf9",
+    "revision": "341e7c9faa93e2c64a4b",
     "url": "/marketplace/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "25ba7e8ad0c055152fb6",
+    "revision": "b4558e423fdaffbe50c2",
     "url": "/marketplace/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "531bb9bfcc5486eccaf9",
-    "url": "/marketplace/static/js/2.10881ab7.chunk.js"
+    "revision": "341e7c9faa93e2c64a4b",
+    "url": "/marketplace/static/js/2.e26ee509.chunk.js"
   },
   {
-    "revision": "3a5359d431b8190dc2f36e4703f39768",
-    "url": "/marketplace/static/js/2.10881ab7.chunk.js.LICENSE.txt"
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/marketplace/static/js/2.e26ee509.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "25ba7e8ad0c055152fb6",
-    "url": "/marketplace/static/js/main.2db08dd3.chunk.js"
+    "revision": "b4558e423fdaffbe50c2",
+    "url": "/marketplace/static/js/main.b62e5174.chunk.js"
   },
   {
     "revision": "770945736d075e31cbfd",
