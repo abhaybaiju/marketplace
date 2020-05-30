15bf98b0c14d1ba3551b1972ae22dfa7ed3208b9
abhay.baiju <abhay0198@gmail.com>
2020-05-30 12:43:50 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,132 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './Home.js';
 import './App.css';
-import { Jumbotron, Container } from 'reactstrap';
+
 import {
-  Collapse,  Navbar,  NavbarToggler,  NavbarBrand,  Nav,  NavItem,  NavLink,  UncontrolledDropdown,  DropdownToggle,  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Row, Col
-} from 'reactstrap';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
   <div>
-    <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler  />
-        <Collapse isOpen={false} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-      <Jumbotron fluid>
-      <Container fluid>
-        <h1 className="display-3">Marketplace</h1>
-        <p className="lead">This is a site description.</p>
-      </Container>
-    </Jumbotron>
-    <Row>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-    </Row>
-    <p></p>
-    <Row>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-      <Col>
-      <Card body>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      </Col>
-    </Row>
-    
+    <Router>
+    <Switch>
+  <Route exact path="/product" component={Product}>
+      </Route>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+    </Switch>
+    </Router>
   </div>
   );
+}
+function Product(props){
+  const {productID} = props.location.state
+  console.log(productID);
+return <h1>Product {productID}</h1>
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import {
+  Collapse,  Navbar,  NavbarToggler,  NavbarBrand,  Nav,  NavItem,  NavLink,  UncontrolledDropdown,  DropdownToggle,  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row, Col
+} from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from './logo.svg';
+
+function Home(){
+    return(
+        <div>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <NavbarToggler  />
+        <Collapse isOpen={false} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+  </Navbar> 
+      <Jumbotron fluid>
+      <Container fluid>
+        <h1 className="display-3">Marketplace</h1>
+        <p className="lead">This is a site description.</p>
+      </Container>
+    </Jumbotron>
+    <Row>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>
+          <Link to={{pathname: '/product', state: { productID: 1}}}>Buy</Link>
+
+            </Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button><Link to={{pathname: '/product', state: { productID: 2}}}>Buy</Link></Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button><Link to={{pathname: '/product', state: { productID: 3}}}>Buy</Link></Button>
+        </CardBody>
+      </Card>
+      </Col>
+    </Row>
+    <p></p>
+    <Row>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button><Link to={{pathname: '/product', state: { productID: 4}}}>Buy</Link></Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button><Link to={{pathname: '/product', state: { productID: 5}}}>Buy</Link></Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card body>
+        <CardImg top width="100%" src={logo} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button><Link to={{pathname: '/product', state: { productID: 6}}}>Buy</Link></Button>
+        </CardBody>
+      </Card>
+      </Col>
+    </Row>
+    
+    </div>
+    );
+}
+
+export default Home;
