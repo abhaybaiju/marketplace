7c3dd4e930f40c4e8d74b9a6daf405b63576413b
abhay.baiju <abhay0198@gmail.com>
2020-05-30 08:37:49 -0700

    Updates

---
+++ precache-manifest.6fd535280ae783a51980b2493a123be9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62764dd78b20bc47113d88cc9ddecae",
+    "url": "/marketplace/index.html"
+  },
+  {
+    "revision": "531bb9bfcc5486eccaf9",
+    "url": "/marketplace/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "25ba7e8ad0c055152fb6",
+    "url": "/marketplace/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "531bb9bfcc5486eccaf9",
+    "url": "/marketplace/static/js/2.10881ab7.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/marketplace/static/js/2.10881ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ba7e8ad0c055152fb6",
+    "url": "/marketplace/static/js/main.2db08dd3.chunk.js"
+  },
+  {
+    "revision": "770945736d075e31cbfd",
+    "url": "/marketplace/static/js/runtime-main.61583d1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marketplace/static/media/logo.5d5d9eef.svg"
+  }
+]);
